12789c38dc3c636d1e734d2dbaa0c236d1521d2c
Wydział Wewnętrzny <35401291+WW5833@users.noreply.github.com>
2025-01-10 00:29:45 +0100

    Initial AddQuestionPage rework

--- src/AddQuestionPage.c
+++ src/AddQuestionPage.c
@@ -6,60 +6,48 @@
 #include <ctype.h>
 #include "IOHelper.h"
 #include "TextHelper.h"
+#include "Popup.h"
 
 #define MAX_QUESTION_LENGTH 256
 
-bool CopyBuffer(char** dest, const char* src, int length) {
-    if(length == 0) {
-        printf("\nTo pole jest wymagane !!\n");
-        return false;
-    }
+typedef struct {
+    int terminalWidth;
+    int terminalHeight;
 
-    if(length == MAX_QUESTION_LENGTH - 2) {
-        int i = length + 1;
-        int read;
-        char buffer[MAX_QUESTION_LENGTH];
-        do {
-            fgets(buffer, MAX_QUESTION_LENGTH, stdin);
-            read = (int)strlen(buffer);
-            i += read - 1;
-        } while(read == MAX_QUESTION_LENGTH - 1);
-        printf("\nLimit znaków wynosi %d, podano %d (polskie/specjalne znaki liczą się za więcej niż jeden znak)!\n", MAX_QUESTION_LENGTH - 3, i);
-        return false;
-    }
+    Question* question;
+    int slotNumber;
 
+    char buffer[MAX_QUESTION_LENGTH];
+    int cursorPosition;
+} AddQuestionPageData;
+
+static void DrawUI(AddQuestionPageData* data);
+
+static void CopyBuffer(char** dest, const char* src, int length) {
     *dest = malloc((size_t)(length + 1));
     for (int i = 0; i < length; i++) {
-        if(src[i] == ';') {
-            printf("\nZnaleziono niedozwolony znak: ';' - spróbuj ponownie\n");
-            free(*dest);
-            *dest = NULL;
-            return false;
-        }
         (*dest)[i] = src[i];
     }
 
     (*dest)[length] = '\0';
-    return true;
 }
 
-int ReadText(char* buffer, int maxLength) {
-    int i = 0;
+static int ReadText(AddQuestionPageData* data, int maxLength) {
     int c;
     do {
         c = getch();
         if(c == '\b') {
-            if(i == 0) continue;
+            if(data->cursorPosition == 0) continue;
 
-            i -= GetCurrentCharSize(buffer, buffer + i - 1);
+            data->cursorPosition -= GetCurrentCharSize(data->buffer, data->buffer + data->cursorPosition - 1);
             printf("\b \b");
             continue;
         }
 
         if(c == '\n' || c == '\r') {
-            buffer[i] = '\0';
+            data->buffer[data->cursorPosition] = '\0';
             printf("\n");
-            return i;
+            return data->cursorPosition;
         }
 
         if(c == ESCAPE_CHAR) {
@@ -68,67 +56,112 @@ int ReadText(char* buffer, int maxLength) {
         }
 
         if(c == ESC) {
-            buffer[0] = '\0';
+            data->buffer[data->cursorPosition] = '\0';
             return -1;
+        }
+
+        if(c == ';') { // Disallow semicolon
+            continue;
         }
 
         if(!(isalnum(c) || ispunct(c) || isspace(c) || (c & 0x80) /* UTF-8 */)) {
             continue;
         }
 
-        buffer[i] = (char)c;
+        data->buffer[data->cursorPosition] = (char)c;
         printf("%c", c);
-        i++;
-    } while(i != maxLength - 1);
+        data->cursorPosition++;
+    } while(data->cursorPosition < maxLength - 1);
 
-    buffer[i] = '\0';
-    return i;
+    data->buffer[data->cursorPosition] = '\0';
+    return data->cursorPosition;
 }
 
-Question *PageEnter_AddQuestion()
-{
-    ClearScreen();
+static bool LoadText(AddQuestionPageData* data, char** output, int* outputLength) {
+    *outputLength = -1;
+    while(*outputLength == -1) {
+        *outputLength = ReadText(data, MAX_QUESTION_LENGTH);
+        if(*outputLength == -1) {
+            HideCursor();
+            if(ShowConfirmationPopup("Czy na pewno chcesz anulować dodawanie pytania?", "Tak", "Nie", 60)) {
+                return false;
+            }
+            ShowCursor();
+            DrawUI(data);
+        }
 
-    Question *question = malloc(sizeof(Question));
-
-    question->Id = GetMaxQuestionId() + 1;
-
-    ShowCursor();
-
-    char buffer[MAX_QUESTION_LENGTH];
-    printf("Dodaj pytanie:\n");
-
-    load_content:
-    printf("Podaj treść pytania: ");
-    question->ContentLength = ReadText(buffer, MAX_QUESTION_LENGTH);
-    if(question->ContentLength == -1) {
-        DestroyQuestion(question);
-        HideCursor();
-        return NULL;
+        if(*outputLength == 0) {
+            ShowAlertPopup("To pole jest wymagane !!", 30);
+            *outputLength = -1;
+            DrawUI(data);
+        }
     }
-    if(!CopyBuffer(&question->Content, buffer, question->ContentLength))
-        goto load_content;
 
+    CopyBuffer(output, data->buffer, *outputLength);
+    data->cursorPosition = 0;
+    return true;
+}
+
+static void DrawUI_Inputed(AddQuestionPageData* data) {
+    if(data->slotNumber == 0) return;
+    printf("Podaj treść pytania: %s\n", data->question->Content);
     for (int j = 0; j < 4; j++)
     {
-        load_answer:
-        if(j == 0)
-            printf("Podaj poprawnej Odpowiedzi: ");
-        else
-            printf("Podaj odpowiedź %c: ", 'A' + j);
-
-        question->AnswerLength[j] = ReadText(buffer, MAX_QUESTION_LENGTH);
-        if(question->AnswerLength[j] == -1) {
-            DestroyQuestion(question);
-            HideCursor();
-            return NULL;
+        if(data->slotNumber == j+1) return;
+        if(j == 0) {
+            printf("Podaj poprawną odpowiedź: %s\n", data->question->Answer[j]);
         }
-        if(!CopyBuffer(&question->Answer[j], buffer, question->AnswerLength[j]))
-            goto load_answer;
+        else {
+            printf("Podaj odpowiedź %c: %s\n", 'A' + j, data->question->Answer[j]);
+        }
+    }
+}
+
+static void DrawUI_Current(AddQuestionPageData* data) {
+    if(data->slotNumber == 0) {
+        printf("Podaj treść pytania: %.*s", data->cursorPosition, data->buffer);
+    } else if(data->slotNumber == 1) {
+        printf("Podaj poprawną odpowiedź: %.*s", data->cursorPosition, data->buffer);
+    } else {
+        printf("Podaj odpowiedź %c: %.*s", 'A' + (data->slotNumber - 1), data->cursorPosition, data->buffer);
+    }
+}
+
+static void DrawUI(AddQuestionPageData* data) {
+    ClearScreen();
+
+    printf("Dodaj pytanie:\n");
+
+    DrawUI_Inputed(data);
+
+    DrawUI_Current(data);
+}
+
+bool InputLoop(AddQuestionPageData* data) {
+    while (data->slotNumber < 5)
+    {
+        DrawUI(data);
+
+        if(data->slotNumber == 0) {
+            if(!LoadText(data, &data->question->Content, &data->question->ContentLength))
+            {
+                return false;
+            }
+        } else {
+
+            if(!LoadText(data, &data->question->Answer[data->slotNumber - 1], &data->question->AnswerLength[data->slotNumber - 1]))
+            {
+                return false;
+            }
+        }
+
+        data->slotNumber++;
     }
 
-    HideCursor();
+    return true;
+}
 
+void AddQuestion(AddQuestionPageData* data) {
     FILE *file = fopen(QUESTIONS_FILE, "a");
     if (file == NULL)
     {
@@ -136,7 +169,7 @@ Question *PageEnter_AddQuestion()
         ExitApp(EXIT_FAILURE);
     }
 
-    AppendQuestion(file, question);
+    AppendQuestion(file, data->question);
 
     if(fclose(file)) {
         perror("Failed to close file");
@@ -145,11 +178,36 @@ Question *PageEnter_AddQuestion()
 
     QuestionListHeader *list = GetQuestionList();
 
-    ListAdd(list, question);
+    ListAdd(list, data->question);
+}
+
+extern int LatestTerminalWidth, LatestTerminalHeight;
+
+Question* PageEnter_AddQuestion()
+{
+    ClearScreen();
+
+    AddQuestionPageData data;
+    data.terminalWidth = LatestTerminalWidth;
+    data.terminalHeight = LatestTerminalHeight;
+    data.slotNumber = 0;
+    data.cursorPosition = 0;
+    data.question = malloc(sizeof(Question));
+    data.question->Id = GetMaxQuestionId() + 1;
+
+    ShowCursor();
+
+    if(!InputLoop(&data)) {
+        free(data.question);
+        return NULL;
+    }
+
+    HideCursor();
+
+    AddQuestion(&data);
 
     printf("\nPytanie dodane pomyślnie\nWciśnij enter aby kontynuować...");
-
     WaitForEnter();
 
-    return question;
+    return data.question;
 }
