2d8521ef996f805f2a74255950b919f6ae8ec86e
Wydział Wewnętrzny <35401291+WW5833@users.noreply.github.com>
2025-01-13 19:17:08 +0100

    Add border in SettingsPage + add colors to values

--- src/SettingsPage.c
+++ src/SettingsPage.c
@@ -6,6 +6,7 @@
 #include "IOHelper.h"
 #include "RGBColors.h"
 #include "TextHelper.h"
+#include "Utf8Symbols.h"
 
 extern Settings* LoadedSettings;
 
@@ -43,10 +44,44 @@ void PrintColors(SettingsPageData* data, int color) {
     }
 }
 
-void PrintUTF8Support(SettingsPageData* data, bool support) {
-    char buffer[256];
-    sprintf(buffer, "Pełne wsparcie UTF-8 | Jeśli ten znak [%c%c%c] (trójkąt) nie jest poprawnie wyświetlany, wyłącz tę opcję | Włączone: %s", 0xE2, 0x96, 0xB6, (support ? "TAK\0" : "NIE\0"));
-    PrintWrappedLine(&buffer[0], data->terminalWidth - 4, 4, false);
+void PrintSettingValue(SettingsPageData* data, int index) {
+    bool value;
+    switch(index) {
+        case 5:
+            SetCursorPosition(data->utf8SupportX, data->utf8SupportY);
+            value = LoadedSettings->FullUTF8Support;
+            break;
+        case 6:
+            SetCursorPosition(data->showCorrectWhenWrongX, data->showCorrectWhenWrongY);
+            value = LoadedSettings->ShowCorrectWhenWrong;
+            SetColorRGBPreset(value ? LoadedSettings->CorrectAnswerColor : LoadedSettings->WrongAnswerColor, false);
+            if(data->terminalWidth - data->showCorrectWhenWrongX < 4) {
+                if(data->terminalWidth - data->showCorrectWhenWrongX < 2) {
+                    printf(CSR_MOVE_LEFT(1));
+                }
+                printf(value ? "T" : "N");
+            }
+            else {
+                printf(value ? "TAK" : "NIE");
+            }
+            ResetColor();
+            return;
+        case 7:
+            SetCursorPosition(data->enableMouseSupportX, data->enableMouseSupportY);
+            value = LoadedSettings->EnableMouseSupport;
+            break;
+        case 8:
+            SetCursorPosition(data->darkModeX, data->darkModeY);
+            value = LoadedSettings->DarkMode;
+            break;
+
+        default:
+            ExitAppWithErrorFormat(EXIT_FAILURE, "[ERROR] Invalid option index: %d\n", data->selected);
+    }
+
+    SetColorRGBPreset(value ? LoadedSettings->CorrectAnswerColor : LoadedSettings->WrongAnswerColor, false);
+    printf(value ? "TAK" : "NIE");
+    ResetColor();
 }
 
 int* GetOptionColor(int selected) {
@@ -71,49 +106,56 @@ int* GetOptionColor(int selected) {
 void DrawSettingsUI(SettingsPageData* data) {
     ResetColor();
     ClearScreen();
-    printf("Ustawienia");
-    printf("\n[ ] Kolor poprawnej odpowiedzi     ");
+
+    PRINT_SINGLE_TOP_BORDER(data->terminalWidth);
+
+    printf("  Ustawienia");
+    printf("\n  [ ] Kolor poprawnej odpowiedzi     ");
     GetCursorPosition(&data->colorsX, &data->colorsY);
 
     data->slimMode = (data->terminalWidth - data->colorsX) < COLOR_PALET_WIDTH;
 
     PrintColors(data, LoadedSettings->CorrectAnswerColor);
-    printf("\n[ ] Kolor błędnej odpowiedzi       ");
+    printf("\n  [ ] Kolor błędnej odpowiedzi       ");
     PrintColors(data, LoadedSettings->WrongAnswerColor);
-    printf("\n[ ] Kolor zaznaczonej odpowiedzi   ");
+    printf("\n  [ ] Kolor zaznaczonej odpowiedzi   ");
     PrintColors(data, LoadedSettings->SelectedAnswerColor);
-    printf("\n[ ] Kolor potwierdzonej odpowiedzi ");
+    printf("\n  [ ] Kolor potwierdzonej odpowiedzi ");
     PrintColors(data, LoadedSettings->ConfirmedAnswerColor);
-    printf("\n[ ] Kolor wsparcia                 ");
+    printf("\n  [ ] Kolor wsparcia                 ");
     PrintColors(data, LoadedSettings->SupportColor);
 
-    printf("\n[ ] ");
+    printf("\n  [ ] ");
+    PrintWrappedLine("Pełne wsparcie UTF-8 | Jeśli ten znak [▶] (trójkąt) nie jest poprawnie wyświetlany, wyłącz tę opcję | Włączone: ___",
+        data->terminalWidth - 8, 6, false);
     GetCursorPosition(&data->utf8SupportX, &data->utf8SupportY);
-    PrintUTF8Support(data, LoadedSettings->FullUTF8Support);
+    data->utf8SupportX -= 3;
+    PrintSettingValue(data, 5);
 
-    printf("\n[ ] Pokaż poprawną odpowiedź po przegranej: ");
+    printf("\n  [ ] ");
+    PrintWrappedLine("Pokaż poprawną odpowiedź po przegranej: ", data->terminalWidth - 8, 6, false);
     GetCursorPosition(&data->showCorrectWhenWrongX, &data->showCorrectWhenWrongY);
-    printf(LoadedSettings->ShowCorrectWhenWrong ? "TAK" : "NIE");
+    PrintSettingValue(data, 6);
 
-    printf("\n[ ] Włącz wsparcie myszki: ");
+    printf("\n  [ ] Włącz wsparcie myszki: ");
     GetCursorPosition(&data->enableMouseSupportX, &data->enableMouseSupportY);
-    printf(LoadedSettings->EnableMouseSupport ? "TAK" : "NIE");
+    PrintSettingValue(data, 7);
 
-    printf("\n[ ] Włącz tryb ciemny: ");
+    printf("\n  [ ] Włącz tryb ciemny: ");
     GetCursorPosition(&data->darkModeX, &data->darkModeY);
-    printf(LoadedSettings->DarkMode ? "TAK" : "NIE");
+    PrintSettingValue(data, 8);
 
     printf("\n");
 
-    printf("\n[ ] Zapisz i wróć do menu głównego");
-    printf("\n[ ] Wróć do menu głównego bez zapisywania");
+    printf("\n  [ ] Zapisz i wróć do menu głównego");
+    printf("\n  [ ] Wróć do menu głównego bez zapisywania");
 
-    data->lineIndexes[0] = 2;
-    data->lineIndexes[1] = 3 + (data->slimMode ? 1 : 0);
-    data->lineIndexes[2] = 4 + (data->slimMode ? 2 : 0);
-    data->lineIndexes[3] = 5 + (data->slimMode ? 3 : 0);
-    data->lineIndexes[4] = 6 + (data->slimMode ? 4 : 0);
-    data->lineIndexes[5] = 7 + (data->slimMode ? 5 : 0);
+    data->lineIndexes[0] = 3;
+    data->lineIndexes[1] = 4 + (data->slimMode ? 1 : 0);
+    data->lineIndexes[2] = 5 + (data->slimMode ? 2 : 0);
+    data->lineIndexes[3] = 6 + (data->slimMode ? 3 : 0);
+    data->lineIndexes[4] = 7 + (data->slimMode ? 4 : 0);
+    data->lineIndexes[5] = 8 + (data->slimMode ? 5 : 0);
 
     data->lineIndexes[6] = data->showCorrectWhenWrongY;
     data->lineIndexes[7] = data->enableMouseSupportY;
@@ -121,19 +163,28 @@ void DrawSettingsUI(SettingsPageData* data) {
     data->lineIndexes[9] = data->darkModeY + 2;
     data->lineIndexes[10] = data->darkModeY + 3;
 
-    SetCursorPosition(2, data->lineIndexes[data->selected]);
+    SetCursorPosition(4, data->lineIndexes[data->selected]);
     printf("*");
+
+    SetCursorPosition(0, data->terminalHeight);
+    PRINT_SINGLE_BOTTOM_BORDER(data->terminalWidth);
+
+    for (int i = 2; i < data->terminalHeight; i++)
+    {
+        SetCursorPosition(data->colorsX, i);
+        PrintGenericBorderEdges(0, data->terminalWidth, i, SINGLE_VERTICAL_LINE, false);
+    }
 }
 
 void HandleArrowUpDownKeys(SettingsPageData* data, int direction) {
-    SetCursorPosition(2, data->lineIndexes[data->selected]);
+    SetCursorPosition(4, data->lineIndexes[data->selected]);
     printf(" ");
 
     data->selected += direction;
     if (data->selected < 0) data->selected = OPTION_COUNT - 1;
     if (data->selected >= OPTION_COUNT) data->selected = 0;
 
-    SetCursorPosition(2, data->lineIndexes[data->selected]);
+    SetCursorPosition(4, data->lineIndexes[data->selected]);
     printf("*");
 }
 
@@ -141,18 +192,12 @@ bool HandleEnterKey(SettingsPageData* data) {
     switch(data->selected) {
         case 5:
             LoadedSettings->FullUTF8Support = !LoadedSettings->FullUTF8Support;
-            SetCursorPosition(data->utf8SupportX, data->utf8SupportY);
-            PrintUTF8Support(data, LoadedSettings->FullUTF8Support);
             break;
         case 6:
             LoadedSettings->ShowCorrectWhenWrong = !LoadedSettings->ShowCorrectWhenWrong;
-            SetCursorPosition(data->showCorrectWhenWrongX, data->showCorrectWhenWrongY);
-            printf(LoadedSettings->ShowCorrectWhenWrong ? "TAK" : "NIE");
             break;
         case 7:
             LoadedSettings->EnableMouseSupport = !LoadedSettings->EnableMouseSupport;
-            SetCursorPosition(data->enableMouseSupportX, data->enableMouseSupportY);
-            printf(LoadedSettings->EnableMouseSupport ? "TAK" : "NIE");
 
             if(!LoadedSettings->EnableMouseSupport) {
                 EnableMouseInput(false);
@@ -161,8 +206,6 @@ bool HandleEnterKey(SettingsPageData* data) {
             break;
         case 8:
             LoadedSettings->DarkMode = !LoadedSettings->DarkMode;
-            SetCursorPosition(data->darkModeX, data->darkModeY);
-            printf(LoadedSettings->DarkMode ? "TAK" : "NIE");
 
             DrawSettingsUI(data);
             break;
@@ -175,6 +218,8 @@ bool HandleEnterKey(SettingsPageData* data) {
             LoadedSettings = LoadSettings(); // Reload settings
             return true;
     }
+
+    PrintSettingValue(data, data->selected);
 
     return false;
 }
