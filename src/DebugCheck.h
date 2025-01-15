046537a8af433fe0361e7a4bb52ca88865f20e41
Wydział Wewnętrzny <35401291+WW5833@users.noreply.github.com>
2025-01-15 21:01:31 +0100

    Disable PROGRAMISTERZY_DEBUG

--- src/DebugCheck.h
+++ src/DebugCheck.h
@@ -4,6 +4,6 @@
 // Comment out the following line to disable debug mode
 
 /// @brief If defined, debug mode is enabled
-#define PROGRAMISTERZY_DEBUG
+// #define PROGRAMISTERZY_DEBUG
 
 #endif // _INC_DEBUGCHECK_H
